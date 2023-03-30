a79f2a08493b32238cec0547e738d23a93418b5f
physiknulpe <thanat0s@web.de>
2023-03-30 12:53:16 +0200

    provisorische Gui hinzugefügt

--- src/main/java/verwaltung/SchuelerListe.java
+++ src/main/java/verwaltung/SchuelerListe.java
@@ -11,14 +11,18 @@ public class SchuelerListe<T extends Schueler> {
 
     ArrayList<Pair<T, Boolean>> list = new ArrayList<>();
 
-    public T getRandom(){
+    public T getRandom() {
         int index = randomWithRange(0, list.size() - 1);
-        if(list.get(index).value) { return list.get(index).key; } else { return getRandom(); }
+        if (list.get(index).value) {
+            return list.get(index).key;
+        } else {
+            return getRandom();
+        }
     }
 
     private int randomWithRange(int min, int max) {
         int range = (max - min) + 1;
-        return (int)(Math.random() * range) + min;
+        return (int) (Math.random() * range) + min;
     }
 
 
